d50a8c085a332f0981bff4f7a376eeac8574d368
RISHITHA  REDDY <RISHITHA  REDDY@Rishithareddypalla>
2024-07-03 15:32:44 +0530

    commit message

---
+++ Corejavaprogram/src/com/tnsif/encapsulation/Encapsulationdemo.java
@@ -0,0 +1,12 @@
+package com.tnsif.encapsulation;
+
+public class Encapsulationdemo {
+	int serialnumber;
+	String name;
+	int age;
+	
+	void show() {
+	System.out.println("serialnumber "+serialnumber+ "name + name + age "); 
+
+}
+}
