ef97ad1f3270c53d0ddcefc551cb8f38d4378adc
RISHITHA  REDDY <RISHITHA  REDDY@Rishithareddypalla.Dlink>
2024-07-10 13:47:18 +0530

    commit message

--- Corejavaprogram/src/com/tnsif/encapsulation/Demo.java
+++ Corejavaprogram/src/com/tnsif/encapsulation/Demo.java
@@ -4,4 +4,11 @@ public class Demo {
 
 
 	public static void main(String[] args) {
-		 encapsulationdemo Encapsulationdemo = new encapsulationdemo();
+		 Encapsulationdemo e= new Encapsulationdemo();
+		 e.serialnumber=1;
+		 e.name="sushma";
+		 e.age=34;
+		 e.show();
+	}
+}
+		 
